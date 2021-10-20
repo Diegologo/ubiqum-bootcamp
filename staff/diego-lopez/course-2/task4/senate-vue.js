949ae8650548d03cf17d80ead8f60abc78bb3976
Diego Lopez <joquecorte@gmail.com>
2021-10-20 15:40:58 +0200

    algún progreso con los filtros

--- staff/diego-lopez/course-2/task4/senate-vue.js
+++ staff/diego-lopez/course-2/task4/senate-vue.js
@@ -1,15 +1,14 @@
-
-
 var app = new Vue(
   {  
   el: '#app',  
   data: {    
     membersArr: [],
+    checkedParties: [],
   },
 
 //fetch
   methods :{
-    plus: function fetchData(){
+    fetchData: function(){
     fetch('https://api.propublica.org/congress/v1/113/senate/members.json',{
     headers:{
       'X-API-Key':'zVc5hB9SJPb7D3LskxNmV0TEicAghQQ4KOETtdFB'
@@ -21,105 +20,27 @@ var app = new Vue(
   }).then(json =>{
     console.log(json);
     app.membersArr = json.results[0].members;
-    console.log(app.membersArr,'ciao')
+    console.log(app.membersArr)
     return app.membersArr
   }).catch((error)=>{
     console.log("no funciona", error);
   })
-  }
   },
+  },
+  computed :{
+    filterRepublicans: function(){
+      return this.membersArr.filter(filter => filter.party === "R")
+    }
+  },
+  filter :{
+
+  }
 }
 );
-app.plus()
-
-
-
-/*
-//build table
-
-function buildTableWhole(membersArr){
-
-  let table = document.getElementById("senate-data");
-  let thead = document.createElement('thead');
-  let tbody = document.createElement('tbody');
-  
-  document.getElementById("senate-data").innerHTML = "";
-
-
-  //build table header
-
-  let th1 = document.createElement('th');
-  let th2 = document.createElement('th');
-  let th3 = document.createElement('th');
-  let th4 = document.createElement('th');
-  let th5 = document.createElement('th');
-
-  let head1 = document.createTextNode("Senator");
-  let head2 = document.createTextNode("Party");
-  let head3 = document.createTextNode("State");
-  let head4 = document.createTextNode("Seniority");
-  let head5 = document.createTextNode("% votes with party");
-
-  th1.appendChild(head1);
-  th2.appendChild(head2);
-  th3.appendChild(head3);
-  th4.appendChild(head4);
-  th5.appendChild(head5);
-
-  thead.appendChild(th1);
-  thead.appendChild(th2);
-  thead.appendChild(th3);
-  thead.appendChild(th4);
-  thead.appendChild(th5);
-
-
-//build table body
-
-  for (let i = 0; i < membersArr.length; i++){
-  
-    let tr = document.createElement('tr');
-      
-  
-  
-    let link = document.createElement('a');
-    let td1 = document.createElement('td');
-    let td2 = document.createElement('td');
-    let td3 = document.createElement('td');
-    let td4 = document.createElement('td');
-    let td5 = document.createElement('td');
-      
-    link.setAttribute('href', membersArr[i].url);
-  
-    let text1 = document.createTextNode(membersArr[i].last_name + " " + membersArr[i].first_name + " " + (membersArr[i].middle_name || ""));
-    let text2 = document.createTextNode(membersArr[i].party);
-    let text3 = document.createTextNode(membersArr[i].state);
-    let text4 = document.createTextNode(membersArr[i].seniority);
-    let text5 = document.createTextNode(membersArr[i].votes_with_party_pct + "%");
-      
-    td1.appendChild(link);
-    link.appendChild(text1);
-    td2.appendChild(text2);
-    td3.appendChild(text3);
-    td4.appendChild(text4);
-    td5.appendChild(text5);
-      
-    tr.appendChild(td1);
-    tr.appendChild(td2);
-    tr.appendChild(td3);
-    tr.appendChild(td4);
-    tr.appendChild(td5);
-  
-    tbody.appendChild(tr);
-    table.appendChild(thead);
-    table.appendChild(tbody);
-  }
-};
-buildTableWhole(membersArr)
-
-*/
-
+app.fetchData()
 
 //listeners
+/*
 document.getElementById("democrat").addEventListener("click", function () {
   return (document.getElementById("democrat").checked === true ? filter_party(membersArr) : filter_party(membersArr));
 });
@@ -159,3 +80,4 @@ function filter_party(){
   }
   buildTableWhole(selectedPartyMembers);
 };
+*/
