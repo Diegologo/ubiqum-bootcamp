78caefa3ee9f67965b31f6c9096c716d5fcca767
Diego López González <diegologo89@gmail.com>
2021-11-08 17:30:12 +0100

    begun task3

---
+++ staff/diego-lopez/course-3/task3/src/components/Home.js
@@ -0,0 +1,21 @@
+const Home = () => {
+    return [Events, Contact]
+}
+
+const Events = 
+<div id="content">
+<p class="date">August 4</p>
+<p class="description">NYSL Fundraiser</p>
+<p class="date">August 16</p>
+<p class="description">Season Kick-off: Meet the Teams</p>
+<p class="date">September 1</p>
+<p class="description">First Game of the Season (Check Game Schedule for details)</p>
+</div>
+
+const Contact = 
+<div id="content">
+<p>Please email us at <a href="mailto:nysl@chisoccer.org">nysl@chisoccer.org</a></p>
+<p id='contact-bottom'>We will reply to your email as soon as we can.</p>
+</div>
+
+export default Home
