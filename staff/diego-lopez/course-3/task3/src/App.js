f87d981e447387310ced23c9d0d261f6b06c886e
Diego López González <diegologo89@gmail.com>
2021-11-11 18:18:18 +0100

    task4 done, added maps

--- staff/diego-lopez/course-3/task3/src/App.js
+++ staff/diego-lopez/course-3/task3/src/App.js
@@ -6,41 +6,40 @@ import NavBar from './components/Navigation';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Schedule from './components/Schedule';
 import GameLoc from './components/GameLoc';
+import logo from './assets/img/nysl_logo.png';
 
 function App() {
   return (
     <Router>
       <div className='container-fluid'>
         <h1>Il cavalieri Davi</h1>
-        <img style={TopBanner} src={banner} alt="cool banner"/>
+        <img className='TopBanner' src={banner} alt="cool banner"/>
         <NavBar/>
         <Switch>
           <Route exact path="/">
             <h2>Upcoming Events</h2>
-            <hr />
+            <hr/>
             <Events/>
           </Route>
           <Route exact path="/Schedule">
             <h2>NYSL Game Information</h2>
-            <hr />
+            <hr/>
             <Schedule/>
           </Route>
           <Route path="/schedule/:id">
             <h2>Game Location</h2>
-            <hr />
+            <hr/>
             <GameLoc/>
           </Route>
         </Switch>
         <h3>Contact Information</h3>
         <Contact/>
+        <div style={{textAlign:'center'}}>
+          <img id="logo" src={logo} alt="logo"/>
+        </div>
       </div>
     </Router>
   );
-};
-
-const TopBanner = {
-  height: '250px',
-  width: '95vw',
 };
 
 export default App;
--- staff/diego-lopez/course-3/task3/src/components/GameLoc.js
+++ staff/diego-lopez/course-3/task3/src/components/GameLoc.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import data from '../assets/games.json'
+import { Link } from "react-router-dom";
 
 const GameLoc = () => {
     const {id} = useParams();
@@ -17,11 +18,19 @@ const GameLoc = () => {
     return (
         <div>
             {Object.keys(GameLocation).map(valor =>
-                <div key = {GameLocation[valor].id}>
-                <p>Match begins at: {GameLocation[valor].time} the day {GameLocation[valor].date}</p>
-                <p>The {GameLocation[valor].teams} teams are participating at: {GameLocation[valor].location} field</p>
+                <div key = {GameLocation[valor].id}><br/>
+                    <p>Match begins at: {GameLocation[valor].time} the {GameLocation[valor].date}</p><br/>
+                    <p>The {GameLocation[valor].teams} teams are participating at: {GameLocation[valor].location} field</p> 
+                    <br/><hr/>
+                    <div>
+                        <p>Map to get to the match;</p>
+                        <iframe src={GameLocation[valor].url} width="100%" height="450" frameBorder="0" style={{border:0}} allowFullScreen="" aria-hidden="false" tabIndex="0"></iframe><hr/>
+                    </div>
                 </div>
             )}
+            <div style={{textAlign:'center'}}>
+                <Link to="/schedule" className="btn btn-primary">Go back to schedules</Link>
+            </div>
         </div>
     );
 };
