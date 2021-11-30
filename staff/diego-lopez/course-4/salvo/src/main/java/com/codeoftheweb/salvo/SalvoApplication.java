6778614d64eead3028fda3fd2032d10b5415de9e
Diego López González <diegologo89@gmail.com>
2021-11-30 18:51:53 +0100

    added sample players

--- staff/diego-lopez/course-4/salvo/src/main/java/com/codeoftheweb/salvo/SalvoApplication.java
+++ staff/diego-lopez/course-4/salvo/src/main/java/com/codeoftheweb/salvo/SalvoApplication.java
@@ -2,12 +2,23 @@ package com.codeoftheweb.salvo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
 public class SalvoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SalvoApplication.class, args);
+		SpringApplication.run(SalvoApplication.class);
 	}
-
+    @Bean
+    public CommandLineRunner initData(PlayerRepository repository) {
+        return args -> {
+            // save a couple of customers
+            repository.save(new Player("Jack Bauer"));
+            repository.save(new Player("Chloe O'Brian"));
+            repository.save(new Player("Kim Bauer"));
+            repository.save(new Player("Tony Almeida"));
+        };
+    }
 }
