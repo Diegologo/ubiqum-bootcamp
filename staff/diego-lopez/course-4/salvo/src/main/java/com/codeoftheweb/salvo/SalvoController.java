d6d04a6a0e155a6f6020fbaf1a6d0d818b6e696c
Diego López González <diegologo89@gmail.com>
2022-01-11 16:54:23 +0100

    added mapping

--- staff/diego-lopez/course-4/salvo/src/main/java/com/codeoftheweb/salvo/SalvoController.java
+++ staff/diego-lopez/course-4/salvo/src/main/java/com/codeoftheweb/salvo/SalvoController.java
@@ -3,7 +3,11 @@ package com.codeoftheweb.salvo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import static java.util.stream.Collectors.toList;
 
 @RestController
 @RequestMapping("/api")
@@ -13,8 +17,15 @@ public class SalvoController {
     private GameRepository gameRepository;
 
     @RequestMapping("/games")
-    public List<Game> getGames() {
-        return gameRepository.findAll();
+    public List<Map<String, Object>> getGames() {
+        return gameRepository.findAll().stream().map(game -> {
+            Map<String, Object> gameMap = new HashMap<>();
+            
+            gameMap.put("creationDate", game.getCreationDate());
+            gameMap.put("id", game.getId());
+
+            return gameMap;
+        }).collect(toList());
     }
 
     @Autowired
