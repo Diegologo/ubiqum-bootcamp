66d65a287b19f8b9d5ed2ec64bda649435489931
Diego López González <diegologo89@gmail.com>
2022-02-02 16:26:39 +0100

    game_view done and working

--- staff/diego-lopez/course-4/salvo/src/main/java/com/codeoftheweb/salvo/SalvoApplication.java
+++ staff/diego-lopez/course-4/salvo/src/main/java/com/codeoftheweb/salvo/SalvoApplication.java
@@ -24,13 +24,15 @@ public class SalvoApplication {
         return args -> {
 
 			//placeholder players
-			Player readyPlayer1 = new Player("Jack Bauer", "Jack@email.com");
-			Player readyPlayer2 = new Player("Chloe O'Brian", "Chloe@email.com");
-			Player readyPlayer3 = new Player("Kim Bauer", "Kim@email.com");
+			Player readyPlayer1 = new Player("Jack Bauer", "j.bauer@ctu.gov");
+			Player readyPlayer2 = new Player("Chloe O'Brian", "c.obrian@ctu.gov");
+			Player readyPlayer3 = new Player("Kim Bauer", "kim_bauer@gmail.com");
+			Player readyPlayer4 = new Player("Tony Almeida", "t.almeida@ctu.gov");
 
 			playerRepository.save(readyPlayer1);
 			playerRepository.save(readyPlayer2);
 			playerRepository.save(readyPlayer3);
+			playerRepository.save(readyPlayer4);
 			
 			//placeholder games
 			Game g1 = new Game();
@@ -45,29 +47,31 @@ public class SalvoApplication {
 
 			//placeholder gameplayers
             GamePlayer gp1 = new GamePlayer(g1, readyPlayer1);
-            GamePlayer gp2 = new GamePlayer(g2, readyPlayer2);
-            GamePlayer gp3 = new GamePlayer(g3, readyPlayer3);
+            GamePlayer gp2 = new GamePlayer(g1, readyPlayer2);
+            GamePlayer gp3 = new GamePlayer(g2, readyPlayer3);
+            GamePlayer gp4 = new GamePlayer(g2, readyPlayer4);
 
 			gamePlayerRepository.save(gp1);
 			gamePlayerRepository.save(gp2);
 			gamePlayerRepository.save(gp3);
+			gamePlayerRepository.save(gp4);
 
 			//placeholder ships
-			List<String> location1 = Arrays.asList("H1", "H2");
-			List<String> location2 = Arrays.asList("H1", "H2");
-			List<String> location3 = Arrays.asList("H1", "H2");
+			List<String> location1 = Arrays.asList("H2", "H3", "H4");
+			List<String> location2 = Arrays.asList("E1", "F1", "G1");
+			List<String> location3 = Arrays.asList("B4", "B5");
 
-			Ship ship1P1 = new Ship("friendship", gp1, location1);
-			Ship ship2P1 = new Ship("relationship", gp1, location2);
-			Ship ship1P2 = new Ship("shipping", gp2, location3);
+			Ship ship1P1 = new Ship("Destroyer", gp1, location1);
+			Ship ship2P1 = new Ship("Submarine", gp1, location2);
+			Ship ship3P1 = new Ship("Patrol Boat", gp2, location3);
 
 			gp1.addShip(ship1P1);
 			gp1.addShip(ship2P1);
-			gp2.addShip(ship1P2);
+			gp1.addShip(ship3P1);
 			
 			shipRepository.save(ship1P1);
 			shipRepository.save(ship2P1);
-			shipRepository.save(ship1P2);
+			shipRepository.save(ship3P1);
         };
     }
 }
--- staff/diego-lopez/course-4/salvo/src/main/java/com/codeoftheweb/salvo/SalvoController.java
+++ staff/diego-lopez/course-4/salvo/src/main/java/com/codeoftheweb/salvo/SalvoController.java
@@ -4,9 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedCaseInsensitiveMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.HttpStatus;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.LinkedHashMap;
+import java.util.Set;
 import static java.util.stream.Collectors.toList;
 
 
@@ -46,8 +53,53 @@ public class SalvoController  {
         return gamePlayerRepository.findAll();
     }
 
+    @RequestMapping(path="/game_view/{gamePlayerId}")
+    public ResponseEntity<Object>getGameView(@PathVariable Long gamePlayerId) {
+    GamePlayer gamePlayer = gamePlayerRepository.findById(gamePlayerId).orElse(null);
+       
+        if(gamePlayer==null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        Game game = gamePlayer.getGame();
+      
+        Set<GamePlayer> gamePlayerSet = game.getGamePlayers();
+        Map<String,Object> dto = new LinkedHashMap<>();
+        dto.put("gameplayer_Id",gamePlayer.getGamePlayerId());
+        dto.put("created",game.getCreationDate());
+        dto.put("game_id",game.getGameId());
+        dto.put("gamePlayers", gamePlayerSet.stream()
+                                            .map( gp -> makeGamePlayerDTO(gp))
+                                            .collect(toList()));
+        dto.put("ships",gamePlayer.getShips().stream()
+                                          .map(s-> makeShipDTO(s))
+                                          .collect(toList()));
+
+        return new ResponseEntity<>(dto, HttpStatus.OK);
+    }
+
     @RequestMapping("/ships")
     public List<Ship> getShips() {
         return shipRepository.findAll();
+    }
+
+    public Map<String,Object> makePlayerDTO(Player player){
+        Map<String,Object> playerDTO = new HashMap<>();
+        playerDTO.put("id", player.getPlayerId());
+        playerDTO.put("email", player.getEmail());
+        return playerDTO;
+    }
+
+    public Map<String,Object> makeGamePlayerDTO(GamePlayer gamePlayer){
+        Map<String,Object> gamePlayerDTO = new HashMap<>();
+        gamePlayerDTO.put("id",gamePlayer.getGamePlayerId());
+        gamePlayerDTO.put("player",makePlayerDTO(gamePlayer.getPlayer()));
+        return gamePlayerDTO;
+    }
+
+    public Map<String,Object> makeShipDTO(Ship ship){
+        Map<String,Object> dto = new LinkedCaseInsensitiveMap<>();
+        dto.put("type", ship.getType());
+        dto.put("location",ship.getShipLocation());
+        return dto;
     }
 }
