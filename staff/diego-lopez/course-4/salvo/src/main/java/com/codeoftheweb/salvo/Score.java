d3e0f05de75e509c6a356e6656265501910ebe3d
Diego López González <diegologo89@gmail.com>
2022-03-06 18:17:44 +0100

    score is done

---
+++ staff/diego-lopez/course-4/salvo/src/main/java/com/codeoftheweb/salvo/Score.java
@@ -0,0 +1,70 @@
+package com.codeoftheweb.salvo;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Score {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private long id;
+    private float  score;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name="game_id")
+    private Game game;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name="player_id")
+    private Player player;
+
+    private Date finishDate;
+
+    public Score(){}
+
+    public Score(Game game, Player player, float score, Date finishDate) {
+        this.game = game;
+        this.player = player;
+        this.score = score;
+        this.finishDate = finishDate;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public float getScore() {
+        return score;
+    }
+
+    public Date getFinishDate() {
+        return finishDate;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public void setScore(float score) {
+        this.score = score;
+    }
+
+
+
+}
